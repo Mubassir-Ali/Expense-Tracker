32099d106003155899c54673d10c6bde13cbf401
Mubassir-Ali <mubassir11@gmail.com>
2021-01-12 14:37:41 +0500

    design complete

--- src/components/AddTransiction.js
+++ src/components/AddTransiction.js
@@ -4,18 +4,18 @@ export const AddTransiction = () => {
     return (
         <>
         <h3>Add New Transiction</h3>
-        <form id="form">
+        <form>
         	<div className="form-container">
-			<label for="text">Text</label>
-			<input type='text' id="text" placeholder="Enter text..."/>
+			<label htmlFor="text">Text</label>
+			<input type='text' placeholder="Enter text..."/>
         </div>
 
 
         <div className="form-container">
-			<label for="amount">Amount<br/>
+			<label htmlFor="amount">Amount<br/>
 				(negative-expense, positive-income)	</label>
 				
-			<input type="number" id="amount" placeholder="Enter Amount"/>
+			<input type="number" placeholder="Enter Amount"/>
 
 			
 
--- src/components/TransictionList.js
+++ src/components/TransictionList.js
@@ -6,7 +6,7 @@ export const TransictionList = () => {
     return (
         <>
         <h3>History</h3>
-        <ul>
+        <ul className="list">
             <li className="minus">
                 Cash <span>-$400</span><button className="delete-btn">x</button>
             </li>
