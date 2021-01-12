c16258a661990469d26c6b74efe23c7d3dc289fc
Mubassir-Ali <mubassir11@gmail.com>
2021-01-12 13:29:21 +0500

    Add Balance, IncomeExpense and Header componets

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {Header} from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses'
+
+
+
+function App() {
+  return (
+    <div> 
+    <Header/>
+    <div className="container">
+      <Balance/>
+    </div>
+    <IncomeExpenses/>
+
+    
+
+
+    </div>
+  );
+}
+
+export default App;
